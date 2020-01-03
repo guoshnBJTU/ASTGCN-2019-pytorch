3277e3d893c01b0b15b6d0daa65090b143b4bcdc
guoshn mac <guoshn@bjtu.edu.cn>
2020-01-03 19:54:23 +0800

    add annotation

--- prepareData.py
+++ prepareData.py
@@ -10,7 +10,7 @@ def search_data(sequence_length, num_of_depend, label_start_idx,
     Parameters
     ----------
     sequence_length: int, length of all history data
-    num_of_depend: int, 依赖的历史时间段的个数
+    num_of_depend: int,
     label_start_idx: int, the first index of predicting target
     num_for_predict: int, the number of points will be predicted for each sample
     units: int, week: 7 * 24, day: 24, recent(hour): 1
@@ -23,7 +23,7 @@ def search_data(sequence_length, num_of_depend, label_start_idx,
     if points_per_hour < 0:
         raise ValueError("points_per_hour should be greater than 0!")
 
-    if label_start_idx + num_for_predict > sequence_length: # 判断了未来值是否存在
+    if label_start_idx + num_for_predict > sequence_length:
         return None
 
     x_idx = []
@@ -38,7 +38,7 @@ def search_data(sequence_length, num_of_depend, label_start_idx,
     if len(x_idx) != num_of_depend:
         return None
 
-    return x_idx[::-1]  #倒序
+    return x_idx[::-1]
 
 
 def get_sample_indices(data_sequence, num_of_weeks, num_of_days, num_of_hours,
@@ -67,17 +67,16 @@ def get_sample_indices(data_sequence, num_of_weeks, num_of_days, num_of_hours,
     target: np.ndarray
             shape is (num_for_predict, num_of_vertices, num_of_features)
     '''
-    week_sample, day_sample, hour_sample = None, None, None  # 如果任意一个依赖的长度是0，则该项为None，具有返回值
+    week_sample, day_sample, hour_sample = None, None, None
 
-    # 判断是否存在label,不存在的话直接返回None
-    if label_start_idx + num_for_predict > data_sequence.shape[0]:  # 判断了未来值是否存在
+    if label_start_idx + num_for_predict > data_sequence.shape[0]:
         return week_sample, day_sample, hour_sample, None
 
-    if num_of_weeks > 0:  # 需要此类依赖
+    if num_of_weeks > 0:
         week_indices = search_data(data_sequence.shape[0], num_of_weeks,
                                    label_start_idx, num_for_predict,
                                    7 * 24, points_per_hour)
-        if not week_indices:  # 但是此类依赖不存在，所以该样本无法构造
+        if not week_indices:
             return None, None, None, None
 
         week_sample = np.concatenate([data_sequence[i: j]
@@ -103,7 +102,7 @@ def get_sample_indices(data_sequence, num_of_weeks, num_of_days, num_of_hours,
         hour_sample = np.concatenate([data_sequence[i: j]
                                       for i, j in hour_indices], axis=0)
 
-    target = data_sequence[label_start_idx: label_start_idx + num_for_predict]  # 不写维度切片的话是按着第一个维度切片
+    target = data_sequence[label_start_idx: label_start_idx + num_for_predict]
 
     return week_sample, day_sample, hour_sample, target
 
@@ -173,7 +172,7 @@ def read_and_generate_dataset(graph_signal_matrix_filename,
     testing_set = [np.concatenate(i, axis=0)
                    for i in zip(*all_samples[split_line2:])]
 
-    train_x = np.concatenate(training_set[:-2], axis=-1)  # (B,N,F,T'),把所有历史依赖都按着时间顺序串起来
+    train_x = np.concatenate(training_set[:-2], axis=-1)  # (B,N,F,T')
     val_x = np.concatenate(validation_set[:-2], axis=-1)
     test_x = np.concatenate(testing_set[:-2], axis=-1)
 
@@ -185,7 +184,6 @@ def read_and_generate_dataset(graph_signal_matrix_filename,
     val_timestamp = validation_set[-1]
     test_timestamp = testing_set[-1]
 
-    # 对x进行归一化处理，不需要对y进行归一化
     (stats, train_x_norm, val_x_norm, test_x_norm) = normalization(train_x, val_x, test_x)
 
     all_data = {
@@ -227,8 +225,7 @@ def read_and_generate_dataset(graph_signal_matrix_filename,
     if save:
         file = os.path.basename(graph_signal_matrix_filename).split('.')[0]
         dirpath = os.path.dirname(graph_signal_matrix_filename)
-        filename = os.path.join(dirpath, file + '_r' + str(num_of_hours) + '_d' + str(num_of_days) + '_w' + str(
-            num_of_weeks)) + '_astcgn'
+        filename = os.path.join(dirpath, file + '_r' + str(num_of_hours) + '_d' + str(num_of_days) + '_w' + str(num_of_weeks)) + '_astcgn'
         print('save file:', filename)
         np.savez_compressed(filename,
                             train_x=all_data['train']['x'], train_target=all_data['train']['target'],
@@ -244,7 +241,6 @@ def read_and_generate_dataset(graph_signal_matrix_filename,
 
 def normalization(train, val, test):
     '''
-    只利用train当中的最大值和最小值进行归一化,归一化到均值为0
     Parameters
     ----------
     train, val, test: np.ndarray (B,N,F,T)
@@ -255,9 +251,8 @@ def normalization(train, val, test):
                                      shape is the same as original
     '''
 
-    assert train.shape[1:] == val.shape[1:] and val.shape[1:] == test.shape[1:] # ensure the num of nodes is the same
-
-    mean = train.mean(axis=(0,1,3), keepdims=True) # the average of the array elements along (batch, nb_nodes, timestamp)
+    assert train.shape[1:] == val.shape[1:] and val.shape[1:] == test.shape[1:]  # ensure the num of nodes is the same
+    mean = train.mean(axis=(0,1,3), keepdims=True)
     std = train.std(axis=(0,1,3), keepdims=True)
     print('mean.shape:',mean.shape)
     print('std.shape:',std.shape)
@@ -271,7 +266,8 @@ def normalization(train, val, test):
 
     return {'_mean': mean, '_std': std}, train_norm, val_norm, test_norm
 
-### prepare dataset
+
+# prepare dataset
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", default='configurations/PEMS08_astgcn.conf', type=str,
                     help="configuration file path")
